d24ec91d5cf3595ac6216414f01ed500b062b345
Jonathan Channon <jonathan.channon@gmail.com>
2015-11-11 23:53:00 +0000

    added tests for json process method

--- JSONProcessor_test.go
+++ JSONProcessor_test.go
@@ -1,6 +1,8 @@
 package negotiator
 
 import (
+	"errors"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,4 +24,59 @@ func TestShouldProcessJSONAcceptHeader(t *testing.T) {
 		result := jsonProcessor.CanProcess(tt.acceptheader)
 		assert.True(t, result, "Should process "+tt.acceptheader)
 	}
+}
+
+func TestShouldSetContentTypeHeader(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	model := struct {
+		Name string
+	}{
+		"Joe Bloggs",
+	}
+
+	jsonProcessor := &jsonProcessor{}
+
+	jsonProcessor.Process(recorder, model)
+
+	assert.Equal(t, "application/json", recorder.HeaderMap.Get("Content-Type"))
+}
+
+func TestShouldSetResponseBody(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	model := struct {
+		Name string
+	}{
+		"Joe Bloggs",
+	}
+
+	jsonProcessor := &jsonProcessor{}
+
+	jsonProcessor.Process(recorder, model)
+
+	assert.Equal(t, "{\"Name\":\"Joe Bloggs\"}", recorder.Body.String())
+}
+
+func TestShouldSet500StatusCodeOnError(t *testing.T) {
+	recorder := httptest.NewRecorder()
+
+	model := &User{
+
+		"Joe Bloggs",
+	}
+
+	jsonProcessor := &jsonProcessor{}
+
+	jsonProcessor.Process(recorder, model)
+
+	assert.Equal(t, 500, recorder.Code)
+}
+
+type User struct {
+	Name string
+}
+
+func (u *User) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("oops")
 }
