ed37036a72d88fd2dfc455f3862a2e7d8c8b5686
Jonathan Channon <jonathan.channon@gmail.com>
2015-11-16 09:49:19 +0000

    added mw to demo

--- demo/main.go
+++ demo/main.go
@@ -5,13 +5,17 @@ import (
 
 	"github.com/jchannon/negotiator"
 	"github.com/jchannon/negotiator/demo/responseprocessors"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 )
 
 func main() {
-
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/custom", customHandler)
-	http.ListenAndServe(":9001", nil)
+	goji.Use(negotiatormw)
+	goji.Get("/", homeHandler)
+	goji.Get("/oneoffnegotiator", customHandler)
+	goji.Get("/multinegotiator", multiNegotiatorHandler)
+	goji.Get("/multinegotiatoragain", multiNegotiatorHandlerAgain)
+	goji.Serve()
 }
 
 func homeHandler(w http.ResponseWriter, req *http.Request) {
@@ -21,12 +25,33 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 
 func customHandler(w http.ResponseWriter, req *http.Request) {
 	user := &user{"Joe", "Bloggs"}
-	//Creating the negotiator could be put in middleware so you don't have to do this in every handler
+	//Creating the negotiator could be done for only required handlers or use middleware for all
 	textplainNegotiator := negotiator.New(&responseprocessors.PlainTextResponseProcessor{})
 	textplainNegotiator.Negotiate(w, req, user)
+}
+
+func multiNegotiatorHandler(c web.C, w http.ResponseWriter, req *http.Request) {
+	user := &user{"Joe", "Bloggs"}
+	mynegotiator := c.Env["negotiator"].(*negotiator.Negotiator)
+	mynegotiator.Negotiate(w, req, user)
+}
+
+func multiNegotiatorHandlerAgain(c web.C, w http.ResponseWriter, req *http.Request) {
+	user := &user{"John", "Doe"}
+	mynegotiator := c.Env["negotiator"].(*negotiator.Negotiator)
+	mynegotiator.Negotiate(w, req, user)
 }
 
 type user struct {
 	Firstname string
 	Lastname  string
+}
+
+func negotiatormw(c *web.C, h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		c.Env["negotiator"] = negotiator.New(&responseprocessors.PlainTextResponseProcessor{})
+
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
 }
